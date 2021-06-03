1085c2242c3656d8d68472edd61548f2f66fc3f6
Hugo <53018953+YannoteX@users.noreply.github.com>
2021-06-03 17:59:57 +0200

    Add files via upload

---
+++ Piece.java
@@ -0,0 +1,80 @@
+import java.util.* ;
+
+public class Piece {
+
+  //******* Les Attributs **********
+
+  boolean couleur;
+  String unicode;
+  Case emplacement;
+  // private String tableau de déplacement;
+  int [] caseDeplacer;
+
+  //******* Les Constructeurs  **********
+
+  public Piece(boolean pieceCouleur, String pieceUnicode, Case positionPiece, int[] cases) {
+    this.couleur = pieceCouleur;
+    this.unicode = pieceUnicode;
+    this.emplacement = positionPiece;
+    this.caseDeplacer = cases;
+  }
+
+  public Piece(boolean pieceCouleur, String pieceUnicode) {
+    this.couleur = pieceCouleur;
+    this.unicode = pieceUnicode;
+    this.caseDeplacer = new int[] {};
+  }
+
+  //*********** Les Getters *************
+
+  public boolean isCouleur() {
+    return this.couleur;
+  }
+
+  public String getUnicode() {
+    return this.unicode;
+  }
+
+  public int[] getCaseDeplacer() {
+    return this.caseDeplacer;
+  }
+
+  //*********** Les Setters *************
+
+  public void setCouleur(boolean newColor) {
+    this.couleur = newColor;
+  }
+  // je sais pas si on a besoin un setter pour unicode
+
+  public void setCases(int[] newCases) {
+    this.caseDeplacer = newCases;
+  }
+  //*********** Les Méthodes *************
+
+  public void calculDeplacementPossible(){
+  }
+
+  public boolean caseExiste(int c) {
+    int i =0;
+    while(i<caseDeplacer.length){
+      if(c==caseDeplacer[i]){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public void addCase(int c) {
+    if (! (caseExiste(c))) {
+      int[] temp = new int[this.caseDeplacer.length+1];
+      temp[temp.length]=c;
+      this.caseDeplacer = temp;
+    }
+  }
+
+  public String toString() {
+    String s = new String("Piece Unicode :" + this.unicode + "/n Couleur du piece :" + this.couleur + "/n Case à deplacer" + this.caseDeplacer);
+    return s;
+  }
+
+}
