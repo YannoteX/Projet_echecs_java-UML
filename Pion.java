3d0d787a2ae5f2199c243fcc1637ccb93be372ee
Dark962 <56709604+Dark962@users.noreply.github.com>
2021-06-03 22:47:42 +0200

    Update Pion.java

--- Pion.java
+++ Pion.java
@@ -1,6 +1,10 @@
 import java.util.* ;
 
-public class Pion  {
+public class Pion extends Piece  {
+  
+  public Pion(boolean pionCouleur, String pionUnicode, Case positionPiece){
+      super(pionCouleur, pionUnicode,positionPiece);
+    }
 
   public Pion(boolean pieceCouleur, String pieceUnicode, Case positionPiece, int[] cases){
     super (pieceCouleur, pieceUnicode, positionPiece, cases);
