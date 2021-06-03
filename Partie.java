92cb6f1088dc9a877bb49a537db3bc3fa7813c72
Dark962 <56709604+Dark962@users.noreply.github.com>
2021-06-03 23:01:00 +0200

    Create Partie.java

---
+++ Partie.java
@@ -0,0 +1,47 @@
+
+
+
+public class Partie {
+   
+    public Echiquier jeu;
+
+    
+    public boolean Joueur;
+
+    public String nomPartie;
+
+
+    public void tourA(boolean Joueur) {
+    }
+
+    public void archiverTour() {
+    }
+
+ 
+    public String chargerPartie(String nomPartie) {
+    }
+
+   
+    public void supprimerPartie() {
+    }
+  
+    public void sauvegarderPartie() { //à partir de l'échequier()
+    }
+
+ 
+    public void roque() {
+    }
+
+   
+    public void démarrer(String nomPartie) {
+    }
+
+
+    public void init(Case tableaux de cases) {
+    }
+
+   
+    public Partie(String nomPartie) {
+    }
+
+}
