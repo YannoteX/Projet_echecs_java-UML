040419ca114ed3ed330ecc19f8e9c717eacb067a
Dark962 <56709604+Dark962@users.noreply.github.com>
2021-06-03 23:05:13 +0200

    Create Tour.java

---
+++ Tour.java
@@ -0,0 +1,15 @@
+
+public class Tour extends Piece {
+   
+    public Pion(boolean pionCouleur, String pionUnicode, Case positionPiece){
+      super(pionCouleur, pionUnicode,positionPiece);
+    }
+
+    public Pion(boolean pieceCouleur, String pieceUnicode, Case positionPiece, int[] cases){
+      super (pieceCouleur, pieceUnicode, positionPiece, cases);
+    }
+
+    public void calculDeplacementPossible() {
+    }
+
+}
